52f8b6527f414f5f31716ff52ff457e5bea26ac1
Konstantin8105 <konstantin8105@gmail.com>
2019-09-29 14:38:14 +0300

    mms: add more information

--- common.go
+++ common.go
@@ -8,14 +8,21 @@ import (
 // Debug is flag for switching to debug mode
 var Debug bool = false
 
-func called() string {
-	function, file, line, _ := runtime.Caller(2)
-	return fmt.Sprintf(
-		"\tFile     : %s\n"+
-			"\tFunction : %s\n"+
-			"\tLine     : %d\n",
-		file,
-		runtime.FuncForPC(function).Name(),
-		line,
-	)
+func called() (info string) {
+	for i := 0; ; i++ {
+		function, file, line, ok := runtime.Caller(i)
+		if !ok {
+			break
+		}
+		info += fmt.Sprintf(
+			"\tFile     : %s\n"+
+				"\tFunction : %s\n"+
+				"\tLine     : %d\n"+
+				"\n",
+			file,
+			runtime.FuncForPC(function).Name(),
+			line,
+		)
+	}
+	return
 }
