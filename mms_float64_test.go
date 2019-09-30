c5c577ecec90fd215721090a4830294cae16de95
Konstantin8105 <konstantin8105@gmail.com>
2019-09-30 16:14:44 +0300

    modify test

--- mms_float64_test.go
+++ mms_float64_test.go
@@ -65,58 +65,72 @@ var sizesExpect = [...]struct {
 const gos = 10
 
 func Test(t *testing.T) {
-	memory := []mm{
-		&Direct{},
-		&Float64sCache{},
-	}
+	oldDebug := Debug
+	defer func() {
+		Debug = oldDebug
+	}()
 
-	var profile [2][100]int64
+	for _, b := range []bool{true} {
+		t.Run(fmt.Sprintf("%v", b), func(t *testing.T) {
+			Debug = b
 
-	for i := range memory {
-		ch := getChan()
-
-		var wg sync.WaitGroup
-		wg.Add(gos)
-		for j := 0; j < gos; j++ {
-			go func() {
-				for amount := range ch {
-					arr := memory[i].Get(amount)
-					// check input data
-					for o := range arr {
-						if arr[o] != 0.0 {
-							t.Errorf("Not zero initialization for %d flow: %e", i, arr[o])
-						}
-					}
-					// change input data
-					for o := range arr {
-						arr[o] = rand.Float64()
-					}
-					size := cap(arr)
-					atomic.AddInt64(&profile[i][size], 1)
-					time.Sleep(time.Millisecond)
-					memory[i].Put(&arr)
-				}
-				wg.Done()
-			}()
-		}
-		wg.Wait()
-	}
-
-	for i := range profile[0] {
-		if profile[0][i] != profile[1][i] {
-			t.Errorf("Size = %d. Not same %d != %d", i, profile[0][i], profile[1][i])
-		}
-	}
-	for i := range sizesExpect {
-		for j := 0; j < 2; j++ {
-			if profile[0][sizesExpect[i].size] != int64(sizesExpect[i].amount) {
-				t.Errorf("not expectd size for pos = %d. %d != %d ",
-					sizesExpect[i].size,
-					profile[0][sizesExpect[i].size],
-					sizesExpect[i].amount*gos,
-				)
+			memory := []mm{
+				&Direct{},
+				&Float64sCache{},
 			}
-		}
+
+			var profile [2][100]int64
+
+			for i := range memory {
+				ch := getChan()
+
+				var wg sync.WaitGroup
+				wg.Add(gos)
+				for j := 0; j < gos; j++ {
+					go func() {
+						for amount := range ch {
+							arr := memory[i].Get(amount)
+							// check input data
+							for o := range arr {
+								if arr[o] != 0.0 {
+									t.Errorf("Not zero initialization for %d flow: %e", i, arr[o])
+								}
+							}
+							// change input data
+							for o := range arr {
+								arr[o] = rand.Float64()
+							}
+							size := cap(arr)
+							if len(arr) != amount || cap(arr) != amount {
+								t.Errorf("Not valid length")
+							}
+							atomic.AddInt64(&profile[i][size], 1)
+							time.Sleep(time.Millisecond)
+							memory[i].Put(&arr)
+						}
+						wg.Done()
+					}()
+				}
+				wg.Wait()
+			}
+
+			for i := range profile[0] {
+				if profile[0][i] != profile[1][i] {
+					t.Errorf("Size = %d. Not same %d != %d", i, profile[0][i], profile[1][i])
+				}
+			}
+			for i := range sizesExpect {
+				for j := 0; j < 2; j++ {
+					if profile[0][sizesExpect[i].size] != int64(sizesExpect[i].amount) {
+						t.Errorf("not expectd size for pos = %d. %d != %d ",
+							sizesExpect[i].size,
+							profile[0][sizesExpect[i].size],
+							sizesExpect[i].amount*gos,
+						)
+					}
+				}
+			}
+		})
 	}
 }
 
@@ -156,93 +170,125 @@ func Benchmark(b *testing.B) {
 }
 
 func TestEmpty(t *testing.T) {
-	var c Float64sCache
-	arr := c.Get(3)
-	if len(arr) != 3 {
-		t.Errorf("not valid len")
-	}
-	arr[0] = 42
-	c.Put(&arr)
-	for _, size := range []int{2, 5, 5, 3, 100, 2, 5, 3, 100} {
-		arr2 := c.Get(size)
-		if len(arr2) != size {
-			t.Errorf("not valid len: %d:%d with size = %d",
-				len(arr2), cap(arr2), size)
+	oldDebug := Debug
+	defer func() {
+		Debug = oldDebug
+	}()
+	for _, b := range []bool{false, true} {
+		Debug = b
+		var c Float64sCache
+		arr := c.Get(3)
+		if len(arr) != 3 {
+			t.Errorf("not valid len")
 		}
-		if arr2[0] != 0 {
-			t.Errorf("not same arrays")
+		arr[0] = 42
+		c.Put(&arr)
+		for _, size := range []int{2, 5, 5, 3, 100, 2, 5, 3, 100} {
+			arr2 := c.Get(size)
+			if len(arr2) != size {
+				t.Errorf("not valid len: %d:%d with size = %d",
+					len(arr2), cap(arr2), size)
+			}
+			if arr2[0] != 0 {
+				t.Errorf("not same arrays")
+			}
+			c.Put(&arr2)
 		}
-		c.Put(&arr2)
 	}
 }
 
 func TestDublicatePutting(t *testing.T) {
+	oldDebug := Debug
+	defer func() {
+		Debug = oldDebug
+	}()
+
+	Debug = true
+
 	defer func() {
 		if r := recover(); r == nil {
-			t.Fatalf("Cannot found dublicate putting")
+			t.Fatalf("Cannot found dublicate putting for Debug = %v", Debug)
 		} else {
 			if testing.Verbose() {
 				fmt.Fprintf(os.Stdout, "\n%v\n", r)
 			}
 		}
 	}()
-	oldDebug := Debug
-	defer func() {
-		Debug = oldDebug
-	}()
-
-	Debug = true
 
 	size := 5
 
 	var c Float64sCache
 	arr := c.Get(size)
+	if len(arr) != size {
+		t.Fatalf("not valid len")
+	}
 	c.Put(&arr)
+	if len(arr) != 0 {
+		t.Fatalf("not valid len")
+	}
 	arr = arr[:size]
+	if len(arr) != size {
+		t.Fatalf("not valid len")
+	}
 	c.Put(&arr)
 }
 
 func TestMemoryAccessAfterPut(t *testing.T) {
-	defer func() {
-		if r := recover(); r == nil {
-			t.Fatalf("Cannot found memory access after putting")
-		}
-	}()
 	oldDebug := Debug
 	defer func() {
 		Debug = oldDebug
 	}()
 
-	Debug = true
+	for _, b := range []bool{true} {
+		t.Run(fmt.Sprintf("%v", b), func(t *testing.T) {
+			Debug = b
 
-	size := 5
+			defer func() {
+				if r := recover(); r == nil {
+					t.Fatalf("Cannot found memory access after putting")
+				} else {
+					if testing.Verbose() {
+						fmt.Fprintf(os.Stdout, "\n%v\n", r)
+					}
+				}
+			}()
 
-	var c Float64sCache
-	arr := c.Get(size)
-	c.Put(&arr)
-	arr[3] = 42
+			size := 5
+			var c Float64sCache
+			arr := c.Get(size)
+			c.Put(&arr)
+			arr[3] = 42
+		})
+	}
 }
 
 func TestReset(t *testing.T) {
-	defer func() {
-		if r := recover(); r != nil {
-			t.Fatalf("Cannot reset cache")
-		}
-	}()
 	oldDebug := Debug
 	defer func() {
 		Debug = oldDebug
 	}()
 
-	Debug = true
+	for _, b := range []bool{false, true} {
+		t.Run(fmt.Sprintf("%v", b), func(t *testing.T) {
+			defer func() {
+				if r := recover(); r != nil {
+					t.Fatalf("Cannot reset cache: %v", r)
+				}
+			}()
 
-	size := 5
+			size := 5
 
-	var c Float64sCache
-	arr := c.Get(size)
-	c.Put(&arr)
-	c.Reset() // reset
-	c.Put(&arr)
+			var c Float64sCache
+			arr := c.Get(size)
+			c.Put(&arr)
+			c.Reset() // reset
+			arr = arr[:size]
+			if len(arr) != size {
+				t.Fatalf("not valid len")
+			}
+			c.Put(&arr)
+		})
+	}
 }
 
 func TestConcept(t *testing.T) {
